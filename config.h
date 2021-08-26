9006891427d46aefe353ac772c04205b01c78dd2
eric.woo <wadahana@gmail.com>
2021-08-26 22:08:02 +0800

    patch dwm-viewontag-20210312-61bb8b2.diff

--- config.h
+++ config.h
@@ -6,6 +6,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "monospace:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true" }; 
 static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
--- dwm.c
+++ dwm.c
@@ -1850,6 +1850,8 @@ tag(const Arg *arg)
 		selmon->sel->tags = arg->ui & TAGMASK;
 		focus(NULL);
 		arrange(selmon);
+		if(viewontag && ((arg->ui & TAGMASK) != TAGMASK))
+			view(arg);
 	}
 }
 
